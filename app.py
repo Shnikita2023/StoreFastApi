f7bd442bfc4f1256cb5a28831aeda2abe86b97b5
Никита Швейцер <sh.nik2019@mail.ru>
2023-06-09 23:56:45 +0500

    Start app

---
+++ app.py
@@ -0,0 +1,13 @@
+from email.header import Header
+from http.cookiejar import Cookie
+from typing import Union, Annotated
+from fastapi import FastAPI
+from pydantic import BaseModel
+from pydantic.networks import HttpUrl
+
+
+app = FastAPI()
+
+
+
+
